e32f9b6b5b21d5acc8013f3c40958ef7ad8263dc
Hailemariam addisu <105885701+haile-12-21-23@users.noreply.github.com>
2025-10-23 22:47:18 +0300

    Define Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader=()=>{
+    return(
+    <div className="flex justify-center items-center h-40">
+        <div className="w-10 h-10 border-4 border-t-4 border-gray-200 rounded-full animate-spin border-t-blue-500">
+
+        </div>
+    </div>);
+};
+export default Loader;
